42a9df16183ab7e9689dd22ccaa2c951b5f3720d
Chris Stillman <chrisstillman@macbook-pro.lan>
2022-07-09 21:43:08 -0500

    added static

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (res,req)=>res.send("Hello"))
+app.use('/',express.static('public'));
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${port}`)
-  })
+  });
